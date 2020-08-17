ea42e0e6f064f6f15b40b97278f64ead53a2cca0
Felix Zhou <felix.rn.zhou@gmail.com>
2020-08-17 04:35:18 -0400

    add tokenizer

---
+++ src/tokenizer.rs
@@ -0,0 +1,82 @@
+use std::fs;
+
+const ASCII_LOWER: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y',
+    'z'
+];
+
+const ASCII_UPPER: [char; 26] = [
+    'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y',
+    'Z'
+];
+
+pub struct Token {
+    pub token_type: TokenType,
+    pub content: String,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, content: String) -> Token {
+        Token {
+            token_type,
+            content,
+        }
+    }
+}
+
+pub enum TokenType {
+    Brace,
+    Parenthesis,
+    Semicolon,
+    Keyword,
+    Identifier,
+    Literal,
+}
+
+
+pub fn generate_tokens(file: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let contents = fs::read_to_string(file).expect("Failed to read file");
+
+    for line in contents.lines() {
+        for word in line.trim().trim_end_matches(';').split(" ") {
+            let word = word.to_string();
+
+            if word == "{" || word == "}" {
+                // Check brace
+                tokens.push(Token::new(TokenType::Brace, word));
+            } else if word == "Return" {
+                // Check Return keywordk (refactor later)
+                tokens.push(Token::new(TokenType::Keyword, word));
+            } else if word.contains("(") && word.contains(")") {
+                // Check function name and paren (refactor)
+                let open_pos = word.find("(").unwrap();
+                let close_pos = word.find(")").unwrap();
+
+                if open_pos < close_pos && open_pos != 0 && close_pos == word.len() - 1 {
+                    tokens.push(Token::new(TokenType::Identifier, word[..open_pos].to_string()));
+                    tokens.push(Token::new(TokenType::Parenthesis, "(".to_string()));
+                    tokens.push(Token::new(TokenType::Parenthesis, ")".to_string()));
+                }
+            } else if word.chars().all(|c| ASCII_LOWER.contains(&c) || ASCII_UPPER.contains(&c)) {
+                // Check for identifier
+                tokens.push(Token::new(TokenType::Identifier, word));
+            }  else if let Ok(_) = word.parse::<u128>() {
+                // Check numeric literal (int)
+                tokens.push(Token::new(TokenType::Literal, word));
+            }
+        }
+        if line.contains(";") {
+            tokens.push(Token::new(TokenType::Semicolon, ";".to_string()));
+        }
+    }
+    tokens
+}
