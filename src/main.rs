32a96efe30eee989cf2269972844c939d6281283
Felix Zhou <felix.rn.zhou@gmail.com>
2020-08-17 04:35:11 -0400

    Checking tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod tokenizer;
+
 fn main() {
-    println!("Hello, world!");
+    let tokens = tokenizer::generate_tokens("examples/sample.c");
+    for token in tokens.iter() {
+        println!("{}", token.content);
+    }
 }
