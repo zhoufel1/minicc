06b0295435f5ee886276ce9d261360c9c4cebf1f
Felix Zhou <felix.rn.zhou@gmail.com>
2020-08-19 00:39:27 -0400

    update gen logic

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,13 +1,17 @@
 use crate::parser::Program;
 
 pub fn compile(ast: &Program) -> String {
+    let mut res = String::new();
     let main = &ast.main;
     let name = main.name.to_string();
+    res.push_str(&format!("    .globl {}\n", name)[..]);
+    res.push_str("main:\n");
     let val = main.body.return_value.val;
-
-    format!("   .globl {n}
-{n}:
-    movl ${r}, %eax
-    ret
-    ", n=name, r=val)
+    res.push_str(&format!("    mov ${}, %eax\n", val)[..]);
+    res.push_str("    ret\n");
+    res
 }
+
+// fn traverse_main(ast: &Program) -> String {
+
+// }
