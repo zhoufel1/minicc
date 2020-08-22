fdf6ccb3312af70d42f0f206e7da49816b924e83
Felix Zhou <felix.rn.zhou@gmail.com>
2020-08-22 01:41:59 -0400

    Update tokenizer for unary operators

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,4 +1,3 @@
-
 pub const KEYWORDS: [&str; 1] = [
     "return"
 ];
@@ -7,20 +6,16 @@ pub const TYPES: [&str; 1] = [
     "int"
 ];
 
-pub const OPERATORS: [&str; 3] = [
-    "-", "~", "!"
-];
-
 pub struct Token {
     pub token_type: TokenType,
-    pub value: String,
+    pub value: String
 }
 
 impl Token {
     pub fn new(token_type: TokenType, value: &str) -> Token {
         Token {
             token_type,
-            value: value.to_string(),
+            value: value.to_string()
         }
     }
 }
@@ -34,7 +29,7 @@ pub enum TokenType {
     Constant,
     Keyword,
     Decl,
-    Op,
+    Op
 }
 
 pub fn generate_tokens(contents: &str) -> Vec<Token> {
@@ -90,13 +85,14 @@ pub fn generate_tokens(contents: &str) -> Vec<Token> {
 
 fn _flush(chars: &mut String, tokens: &mut Vec<Token>) {
     if !chars.is_empty() {
-        if let Ok(_) = chars.parse::<u8>() {
+        if let Ok(_) = chars.parse::<u32>() {
             tokens.push(Token::new(TokenType::Constant, chars));
         } else if TYPES.contains(&&chars[..]) {
             tokens.push(Token::new(TokenType::Decl, chars));
         } else if KEYWORDS.contains(&&chars[..]) {
             tokens.push(Token::new(TokenType::Keyword, chars));
         } else {
+            // TODO: This should check if the identifier is valid
             tokens.push(Token::new(TokenType::Identifier, chars));
         }
         chars.clear();
