d7bc9c3c9b00b282a456b076364ca7d1d6f736c0
zhoufel1 <felix.rn.zhou@gmail.com>
2020-08-28 04:00:29 -0400

    refactor write asm into functions

--- src/gen.rs
+++ src/gen.rs
@@ -3,22 +3,30 @@ use std::fs;
 use std::io::Write;
 
 pub fn write_assembly(tree: &ast::Program) {
-    let mut file = fs::OpenOptions::new()
+    let file = fs::OpenOptions::new()
         .create(true)
         .append(true)
-        .open("test.s")
+        .open("result.s")
         .expect("Failed to open file");
 
     //Write default boilerplate
     let tree = &tree.main;
+    generate_function(&file, &tree);
+
+    let tree = &tree.body;
+    generate_return(&file, &tree);
+}
+
+fn generate_function(mut file: &fs::File, tree: &ast::Function) {
     let name = &tree.name;
 
     writeln!(&mut file, "        .globl  {}", name)
         .expect("Failed to write to file");
     writeln!(&mut file, "{}: ", name)
         .expect("Failed to write to file");
+}
 
-    let tree = &tree.body;
+fn generate_return(mut file: &fs::File, tree: &ast::Return) {
     let ret_value = match &tree.return_value {
         ast::Expr::Const { int } => {
             int.val
