0327b2feec9ddf3dab7a483de0c1d342f43a11f2
Felix Zhou <felix.rn.zhou@gmail.com>
2020-08-28 02:27:46 -0400

    Remove code for positon

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-pub const KEYWORDS: [&str; 2] = [
+const KEYWORDS: [&str; 2] = [
     "return", "int"
 ];
 
@@ -32,49 +32,45 @@ pub enum TokenType {
 
 pub fn generate_tokens(contents: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
-    let mut line_pos: u32 = 1;
 
     for line in contents.lines() {
         let mut char_buffer = String::new();
-        let mut col_pos = 1;
 
         for ch in line.trim().chars() {
             let ch_s = &ch.to_string()[..];
             match ch {
-                ' ' => _flush(&mut char_buffer, &mut tokens),
+                ' ' => flush(&mut char_buffer, &mut tokens),
                 '('
                 | ')' => {
-                    _flush(&mut char_buffer, &mut tokens);
+                    flush(&mut char_buffer, &mut tokens);
                     tokens.push(Token::new(TokenType::Paren, ch_s));
                 },
                 '{'
                 | '}' => {
-                    _flush(&mut char_buffer, &mut tokens);
+                    flush(&mut char_buffer, &mut tokens);
                     tokens.push(Token::new(TokenType::Brace, ch_s));
                 },
                 ';' => {
-                    _flush(&mut char_buffer, &mut tokens);
+                    flush(&mut char_buffer, &mut tokens);
                     tokens.push(Token::new(TokenType::Semicolon, ch_s));
                 },
                 '-'
                 | '~'
                 | '!' =>  {
-                    _flush(&mut char_buffer, &mut tokens);
+                    flush(&mut char_buffer, &mut tokens);
                     tokens.push(Token::new(TokenType::Op, ch_s));
                 },
                 _ =>  {
                     char_buffer.push(ch);
                 }
             };
-            col_pos += 1;
         }
-        _flush(&mut char_buffer, &mut tokens);
-        line_pos += 1;
+        flush(&mut char_buffer, &mut tokens);
     }
     tokens
 }
 
-fn _flush(chars: &mut String, tokens: &mut Vec<Token>) {
+fn flush(chars: &mut String, tokens: &mut Vec<Token>) {
     if !chars.is_empty() {
         if let Some(_) = Regex::new(r"^[0-9]+$").unwrap().captures(&chars) {
             tokens.push(Token::new(TokenType::Literal, chars))
